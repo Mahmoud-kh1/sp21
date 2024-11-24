4116cb4853898270d6a90a6175671f3175cb55da
Mahmoud-kh1 <hhh116716@gmail.com>
2024-11-24 17:36:41 +0200

    done

--- proj0/game2048/InputSource.java
+++ proj0/game2048/InputSource.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package game2048;
 
 /** Describes a source of input commands.
@@ -15,21 +14,3 @@ interface InputSource {
 
 }
 
-=======
-package game2048;
-
-/** Describes a source of input commands.
- *  @author P. N. Hilfinger
- */
-interface InputSource {
-
-    /** Returns one command string. */
-    String getKey();
-
-    /** Returns a candidate Tile whose row and column is in the range
-     *  0 .. SIZE-1.  */
-    Tile getNewTile(int size);
-
-}
-
->>>>>>> 160747451c147c59d8e3cbf70a7afee2b73bebdb
